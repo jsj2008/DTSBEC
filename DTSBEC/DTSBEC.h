5ddda346e0faac598a7aed241e12de892ddc6fdb
waynett <wanww2011@gmail.com>
2017-03-01 17:31:56 +0800

    Initial Commit

---
+++ DTSBEC/DTSBEC.h
@@ -0,0 +1,19 @@
+//
+//  DTSBEC.h
+//  DTSBEC
+//
+//  Created by waynett on 3/1/17.
+//  Copyright © 2017 dengtacj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DTSBEC.
+FOUNDATION_EXPORT double DTSBECVersionNumber;
+
+//! Project version string for DTSBEC.
+FOUNDATION_EXPORT const unsigned char DTSBECVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DTSBEC/PublicHeader.h>
+
+
